74208cca5f26f29ab9a7947b24f5503a6394c129
Angel Negrete <anganevagg@protonmail.com>
2022-11-11 22:19:00 -0600

    feature: added basic db models

--- main.py
+++ main.py
@@ -11,14 +11,34 @@ app = Flask(__name__)
 app config
 """
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:password@localhost/Veriburguir"
 app.debug = True
-
 
 """
 db definition
 """
 db = SQLAlchemy(app)
+db.init_app(app)
+
+class Client(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	full_name = db.Column(db.String(50), nullable = True)
+
+class Complement(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable = False)
+	category_id = db.Column(db.Integer)
+
+class Order(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	client_id = db.Column(db.Integer, db.ForeignKey("client.id"),  nullable = False)
+
+class Burger(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable = False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods = ['GET'])
 def index():
@@ -30,4 +50,5 @@ def index():
 def orders(id=None):
 	if(id):
 		return id
-	return "orders"
+	
+	return []
