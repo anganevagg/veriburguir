26ce4368ac46aa24119ab50c1f2e9121dc76d714
Monico Torres <asaelgarcia45@gmail.com>
2022-11-24 21:06:14 -0600

    algunos cambios en el front

--- veriburguer-frontend/src/App.jsx
+++ veriburguer-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import { NavBar } from './components/NavBar'
+import  Carrusel  from  './components/Carrousel'
 
 function App() {
   return (
     <div className="App">
 			<NavBar></NavBar>
+     <Carrusel></Carrusel>
     </div>
   )
 }
